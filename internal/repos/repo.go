f9e66a9ff8e5892a47e88443f404e28545592e0d
ruziba3vich <rruzibayevich@gmail.com>
2025-02-24 20:28:45 +0500

    fix repo: fixed repository logic

--- internal/repos/repo.go
+++ internal/repos/repo.go
@@ -1,1 +1,14 @@
 package repos
+
+import "github.com/ruziba3vich/music_lib/internal/models"
+
+type (
+	Repo interface {
+		CreateSong(*models.Song) error
+		DeleteSong(string) error
+		GetSongByID(string) (*models.Song, error)
+		GetSongLyricsPaginated(string, int, int) ([]string, error)
+		GetSongs(map[string]any, int, int) ([]models.Song, error)
+		UpdateSong(*models.Song) error
+	}
+)
